40bff5a40b32bc2abae05c5313bf9a7c6266b405
K <keily.calle@utec.edu.pe>
2025-06-07 19:17:00 -0500

    a

---
+++ cmake-build-debug/CMakeFiles/proyectocCLAB.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proyectocCLAB.dir/main.cpp.obj"
+  "CMakeFiles/proyectocCLAB.dir/main.cpp.obj.d"
+  "libproyectocCLAB.dll.a"
+  "proyectocCLAB.exe"
+  "proyectocCLAB.exe.manifest"
+  "proyectocCLAB.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proyectocCLAB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
