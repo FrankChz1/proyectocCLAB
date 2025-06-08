2e90ccce6b8e6dd551c66d811ce97def24f6c73d
K <keily.calle@utec.edu.pe>
2025-06-07 19:12:40 -0500

    !

--- main.cpp
+++ main.cpp
@@ -4,4 +4,10 @@ using namespace std;
 int main () {
     cout << "Hello World!" << endl;
     cout <<"aaaaaaaaaa\n";
+    cout <<"Hola como estan";
+    cout <<"segundo commit no se que hacer xd\n";
+    cout <<"Revisado";
+    int n;
+    cout << "Ingrese el numero de sucursales:";
+    cin >> n;
 }
