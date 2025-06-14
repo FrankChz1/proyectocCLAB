26cbcd7289811c7d0f1708db05199d0276008136
K <keily.calle@utec.edu.pe>
2025-06-13 21:24:03 -0500

    avance

---
+++ CSucursales.h
@@ -0,0 +1,15 @@
+#ifndef CSUCURSALES_H
+#define CSUCURSALES_H
+
+class CSurcursales {
+private:
+    vector <CProducto> productos;
+    string nombreSurcursal;
+
+public:
+    CSurcursales(): nombreSurcursal("") {}
+
+};
+
+
+#endif //CSUCURSALES_H
--- main.cpp
+++ main.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
 using namespace std;
-class Csurcursales {
-private:
-    string nombreSurcursal;
-public:
-    Csurcursales(): nombreSurcursal("") {};
-};
-int main () {
-    cout << "Hello World!" << endl;
-    cout <<"aaaaaaaaaa\n";
-    cout <<"Hola como estan";
-    cout <<"segundo commit no se que hacer xd\n";
-    cout <<"Revisado";
-    int n;
-    cout << "\nIngrese el numero de sucursales:";
-    cin >> n;
 
+int main () {
+    int n;
+    int opcion;
+    while (true) {
+        cout <<"Menu:\n";
+        cout <<"1. Ingresar sucursales.\n";
+        cout <<"2. Salir.\n";
+        cout <<"Opcion:";
+        cin >> opcion;
+        cin.ignore();
+
+        if (opcion == 2) {
+            return 0;
+        }
+        cout <<"\nNumero de sucursales:";
+        cin >> n;
+        cin.ignore();
+
+        for (int i = 0; i < n; i++) {
+            int cant;
+            cout<< "\nCantidad de productos de sucursal "<<i+1<<":";
+            cin>>cant;
+            cin.ignore();
+
+            for (int j = 0; j < cant; j++) {
+                string producto;
+                cout<<"Producto "<<j+1<<":";
+                cin>>producto;
+                //mas datos
+            }
+        }
+    }
+
+    return 0;
 }
