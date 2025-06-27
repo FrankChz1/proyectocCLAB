9813c90530a85111372d8a238f8442a6e11f31aa
FrankChz1 <frankchuquispuma199@gmail.com>
2025-06-27 03:40:38 -0500

    Nueva actualizacion

--- CSucursal.h
+++ CSucursal.h
@@ -1,15 +1,26 @@
 #ifndef CSUCURSALES_H
 #define CSUCURSALES_H
 
+// 1. Incluir las cabeceras necesarias
+#include <vector>
+#include <string>
+#include "CProducto.h" // 2. Incluir la definición de la clase CProducto
+
+// Es una buena práctica usar el espacio de nombres std
+using std::vector;
+using std::string;
+// O bien, usar std::vector y std::string directamente en el código.
+
 class CSurcursales {
 private:
-    vector <CProducto> productos;
+    vector<CProducto> productos;
     string nombreSurcursal;
 
 public:
-    CSurcursales(): nombreSurcursal("") {}
+    // Constructor por defecto
+    CSurcursales() : nombreSurcursal("") {}
 
+    // ¡A esta clase le falta funcionalidad! (Ver sección 2)
 };
-
 
 #endif //CSUCURSALES_H
